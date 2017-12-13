be3e3bd7fff8b4bfe5bbac1708c0a6489ac30b15
geeeeeeeek <lovezww2011@gmail.com>
2017-12-13 21:23:36 +0800

    updated some as settings

---
+++ app/src/main/java/com/geeeeeeeek/grouprecyclerview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.geeeeeeeek.grouprecyclerview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.geeeeeeek.grouprecyclerview.R;
+
+/**
+ * Created by geeeeeeeek
+ * Date: 13/12/2017
+ * Time: 9:19 PM
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
